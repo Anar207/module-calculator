0f331b6ea1f8f8d8a216d971b4bc49f2bafb5154
Anar207 <2mwhbwfv6d@privaterelay.appleid.com>
2026-05-22 20:17:58 +0300

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,15 @@
+def calculate(a, b, op):
+    if op == '+':
+        return a + b
+    elif op == '-':
+        return a - b
+    elif op == '*':
+        return a * b
+    elif op == '/':
+        if b == 0:
+            return None
+        return a / b
+    elif op == '^':
+        return a ** b
+    else:
+        return None
